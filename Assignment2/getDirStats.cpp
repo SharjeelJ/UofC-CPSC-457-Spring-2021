14d64b7e4acfa3522ef57dc279096397a21dec4d
Sharjeel Junaid <sharjeelj@outlook.com>
2021-05-22 03:26:03 -0600

    Updated getDirStats.cpp - Added default values for the Results struct - Re-added in the provided placeholder (dummy) result values to trigger the print out in main.cpp - popen will now use the -b (brief) flag for file (as suggested in the assignment) - Adjusted the popen print out statement to reflect the above chage - Added in checks to keep track of the file path and size of the largest file encountered - Added in counters to keep track of the total number of directories and files encountered (not implemented correctly and is not accurate)

--- Assignment2/getDirStats.cpp
+++ Assignment2/getDirStats.cpp
@@ -35,38 +35,80 @@ is_dir(const std::string &path) {
 //
 Results
 getDirStats(const std::string &dir_name, int n) {
-    // Creates a new variable of the struct Results to store the all the info of the specified directory recursively
+    // Creates a new variable of the struct Results to store all the info of the specified directory recursively
     Results results;
 
-    // Sets the results boolean to default to invalid (will be changed to valid once all the parsing successfully completes)
+    // Sets the default values for the results data struct
     results.valid = false;
+    results.largest_file_path = "";
+    results.largest_file_size = -1;
+    results.n_files = 0;
+    results.n_dirs = 0;
+    results.all_files_size = 0;
+
+    // If the passed in directory is not actually a valid directory, returns the results as is (mentioning directory is not valid in main.cpp)
+    if (!is_dir(dir_name)) return results;
+
+    // TODO: Remove below fake results
+
+    // prepare a fake results
+//    results.largest_file_path = dir_name + "/some_dir/some_file.txt";
+//    results.largest_file_size = 123;
+//    results.n_files = 321;
+//    results.n_dirs = 333;
+//    results.all_files_size = 1000000;
+
+    std::string type1 = "C source";
+    int count1 = 5;
+    results.most_common_types.push_back({type1, count1});
+    results.most_common_types.push_back({"makefile script", 4});
+    results.most_common_types.push_back({"C++ source", 2});
+    results.most_common_types.push_back({"PNG image", 1});
+
+    results.most_common_words.push_back({"hello", 3});
+    results.most_common_words.push_back({"world", 1});
+
+    std::vector<std::string> group1;
+    group1.push_back(dir_name + "/file1.cpp");
+    group1.push_back(dir_name + "/lib/sub/other.c");
+    results.duplicate_files.push_back(group1);
+    std::vector<std::string> group2;
+    group2.push_back(dir_name + "/readme.md");
+    group2.push_back(dir_name + "/docs/readme.txt");
+    group2.push_back(dir_name + "/x.y");
+    results.duplicate_files.push_back(group2);
+
+    // Updates the boolean to reflect that the directory's info is valid (complete)
+    results.valid = true;
+
+    // TODO: Remove above fake results
 
     // Creates a stack that will store a list of all the files/folders in the current directory and their contents recursively
     vector<string> itemsToParseStack;
 
-    // Adds the current directory to the top of the stack (bottom of the vector)
+    // Adds the current directory (root) to the top of the stack (bottom of the vector)
     itemsToParseStack.push_back(dir_name);
 
-    // Loops through anything remaining in the stack and looks through them recursively
+    // Loops through anything remaining in the stack and looks through them recursively if possible
     while (!itemsToParseStack.empty()) {
         // Stores the reference to the file path of the item at the top of the stack
-        auto currentTopItemName = itemsToParseStack.back();
+        auto currentTopItem = itemsToParseStack.back();
 
         // Removes the item from the top of the stack (file path of the file/folder to examine)
         itemsToParseStack.pop_back();
 
         // Prints out the file path of the current file/folder being examined
-//        printf("%s\n", currentTopItemName.c_str());
+//        printf("%s\n", currentTopItem.c_str());
 
         // Opens the file/folder at the path popped from the stack
-        DIR *currentTopItem = opendir(currentTopItemName.c_str());
+        DIR *currentTopItemData = opendir(currentTopItem.c_str());
 
         // If the file path is a directory then loops through its contents as well
-        if (currentTopItem) {
+        if (currentTopItemData) {
             // Loops through all the contents of the current top directory being examined
             while (1) {
                 // Pointer value to the next sub item
-                dirent *nextSubItem = readdir(currentTopItem);
+                dirent *nextSubItem = readdir(currentTopItemData);
 
                 // If the pointer is not accessible (indicates the end of the folder) then breaks the loop
                 if (!nextSubItem) break;
@@ -77,15 +119,20 @@ getDirStats(const std::string &dir_name, int n) {
                 // Skips this loop iteration for the first 2 values we receive (useless data)
                 if (currentSubItemName == "." || currentSubItemName == "..") continue;
 
-                // Adds the current sub-item's full path as the new parent directory for the next directory to be examined
-                string path = currentTopItemName + "/" + currentSubItemName;
+                // Creates a string of the complete path of the current sub-item
+                string path = currentTopItem + "/" + currentSubItemName;
 
-                // Pushes the new file path to the top of the stack
+                // Pushes the current sub-item's file path to the top of the stack to be checked later for its own subdirectories
                 itemsToParseStack.push_back(path);
             }
-            // Closes the current file/folder
-            closedir(currentTopItem);
+            // Closes the current directory
+            closedir(currentTopItemData);
+
+            // Increments the counter keeping track of the total number of directories encountered
+            results.n_dirs++;
         }
+        // Increments the counter keeping track of the total number of files encountered
+        results.n_files++;
 
         // Creates a new file stream that will store the data from popen
         FILE *fileData;
@@ -94,71 +141,36 @@ getDirStats(const std::string &dir_name, int n) {
         char popenData[PATH_MAX];
 
         // Sets the filestream to contain the data from the popen command
-        fileData = popen(("file " + currentTopItemName).c_str(), "r");
+        fileData = popen(("file -b " + currentTopItem).c_str(), "r");
 
-        // Loops through all the data obtained from popen and prints it to the console
-        while (fgets(popenData, PATH_MAX, fileData) != NULL)
-            printf("%s", popenData);
+        // Prints the data obtained from popen
+        while (fgets(popenData, PATH_MAX, fileData) != NULL) {
+            printf(popenData);
+        }
 
         // Closes popen and the file stream
         pclose(fileData);
 
-        // Creates a stat struct to get the current file's size
+        // Creates a stat struct that will be used to get the current file's size
         struct stat buffer;
 
         // Prints out the data obtained through stat
-        if (stat(currentTopItemName.c_str(), &buffer) == 0) {
-            printf("Last modification: %s", ctime(&buffer.st_mtime));
-            printf("File size: %lld bytes\n", (long long) buffer.st_size);
-            printf("\n");
+        if (stat(currentTopItem.c_str(), &buffer) == 0) {
+//            printf("Last modification: %s", ctime(&buffer.st_mtime));
+//            printf("File size: %lld bytes\n", (long long) buffer.st_size);
+//            printf("\n");
         }
+
+        // Checks to see if the current file is the largest file we have encountered and stores its data if it is
+        if (buffer.st_size > results.largest_file_size) {
+            results.largest_file_size = buffer.st_size;
+            results.largest_file_path = currentTopItem;
+        }
+
+        // Adds the current file size to the existing total file size of the specified directory in the results struct
+        results.all_files_size += buffer.st_size;
     }
+
     // Returns the complete directory info
     return results;
-
-    // The results below are all hard-coded, to show you all the fields
-    // you need to calculate. You should delete all code below and
-    // replace it with your own code.
-
-    // Custom data structure consisting of the directory's information
-    Results res;
-
-    // Stores a boolean to keep track of whether or not the directory's information is update to date and complete (valid)
-    res.valid = false;
-
-    // If the passed in directory is not actually a valid directory, returns the results as is (mentioning directory is not valid in main.cpp)
-    if (!is_dir(dir_name)) return res;
-
-    // prepare a fake results
-    res.largest_file_path = dir_name + "/some_dir/some_file.txt";
-    res.largest_file_size = 123;
-    res.n_files = 321;
-    res.n_dirs = 333;
-    res.all_files_size = 1000000;
-
-    std::string type1 = "C source";
-    int count1 = 5;
-    res.most_common_types.push_back({type1, count1});
-    res.most_common_types.push_back({"makefile script", 4});
-    res.most_common_types.push_back({"C++ source", 2});
-    res.most_common_types.push_back({"PNG image", 1});
-
-    res.most_common_words.push_back({"hello", 3});
-    res.most_common_words.push_back({"world", 1});
-
-    std::vector<std::string> group1;
-    group1.push_back(dir_name + "/file1.cpp");
-    group1.push_back(dir_name + "/lib/sub/other.c");
-    res.duplicate_files.push_back(group1);
-    std::vector<std::string> group2;
-    group2.push_back(dir_name + "/readme.md");
-    group2.push_back(dir_name + "/docs/readme.txt");
-    group2.push_back(dir_name + "/x.y");
-    res.duplicate_files.push_back(group2);
-
-    // Updates the boolean to reflect that the directory's info is valid (complete)
-    res.valid = true;
-
-    // Returns the valid directory info
-    return res;
 }
