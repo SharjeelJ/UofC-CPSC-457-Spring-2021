908e2d7f492647eb568be11bcdb93ff77938e10c
Sharjeel Junaid <sharjeelj@outlook.com>
2021-05-29 22:29:06 -0600

    Updated detectPrimes.cpp - Updated the threaded work function to only return a boolean (of whether or not the number was a prime) - Added code to assign work to threads sequentially until all the numbers in the passed in vector have been evaluated

--- Assignment3/detectPrimes/detectPrimes.cpp
+++ Assignment3/detectPrimes/detectPrimes.cpp
@@ -11,30 +11,25 @@ struct threadParameters {
     int64_t inputNumber;
 };
 
-struct threadReturn {
-    int64_t inputNumber;
-    bool isPrime;
-};
-
 void *threadedWork(void *input) {
     int64_t n = ((threadParameters *) input)->inputNumber;
 
     // handle trivial cases
-    if (n < 2) return (void *) new threadReturn{n, false};
-    if (n <= 3) return (void *) new threadReturn{n, true}; // 2 and 3 are primes
-    if (n % 2 == 0) return (void *) new threadReturn{n, false}; // handle multiples of 2
-    if (n % 3 == 0) return (void *) new threadReturn{n, false}; // handle multiples of 3
+    if (n < 2) return (void *) false;
+    if (n <= 3) return (void *) true; // 2 and 3 are primes
+    if (n % 2 == 0) return (void *) false; // handle multiples of 2
+    if (n % 3 == 0) return (void *) false; // handle multiples of 3
     // try to divide n by every number 5 .. sqrt(n)
     int64_t i = 5;
     int64_t max = sqrt(n);
     while (i <= max) {
-        if (n % i == 0) return (void *) new threadReturn{n, false};
-        if (n % (i + 2) == 0) return (void *) new threadReturn{n, false};
+        if (n % i == 0) return (void *) false;
+        if (n % (i + 2) == 0) return (void *) false;
         i += 6;
     }
 
     // didn't find any divisors, so it must be a prime
-    return (void *) new threadReturn{n, true};
+    return (void *) true;
 }
 
 // returns true if n is prime, otherwise returns false
@@ -79,29 +74,41 @@ vector<int64_t> detect_primes(const vector<int64_t> &nums, int n_threads) {
         // Creates an array of threads based on the required number of threads that was computed
         pthread_t threadsArray[n_threads];
 
-        // Loop to assign work to each of the threads
-        for (int currentThreadIndex = 0; currentThreadIndex < nums.size(); currentThreadIndex++) {
-            pthread_create(&threadsArray[currentThreadIndex], nullptr, threadedWork,
-                           (void *) new threadParameters{(int64_t) nums[currentThreadIndex]});
-        }
+        // Creates a counter that will store the index of the next number to check
+        int nextNumberToCheck = 0;
 
-        // Loop to garbage collect all the threads
-        for (int currentThreadIndex = 0; currentThreadIndex < nums.size(); currentThreadIndex++) {
-            // Creates a pointer that will store the resulting local counter information from the thread
-            void *threadResult = nullptr;
+        // Loop that runs until all the numbers have been checked
+        while (nextNumberToCheck < nums.size()) {
+            // Stores the number of threads that are active (will be used to garbage collect later)
+            int threadsActive = 0;
 
-            // Closes the thread and adds its stores its returned result
-            pthread_join(threadsArray[currentThreadIndex], &threadResult);
+            // Loop to assign work to each of the threads
+            for (int currentThreadIndex = 0;
+                 currentThreadIndex < n_threads && nextNumberToCheck < nums.size(); currentThreadIndex++) {
+                pthread_create(&threadsArray[currentThreadIndex], nullptr, threadedWork,
+                               (void *) new threadParameters{(int64_t) nums[nextNumberToCheck]});
 
-            // Increments the main counter with the thread's localized counter (merges the results from the threads)
-            reinterpret_cast<uint64_t>(threadResult);
-//            reinterpret_cast<threadReturn>(threadResult);
-//            result.push_back(threadResult);
-        }
+                // Increments the index of the next number to check
+                nextNumberToCheck++;
 
-        for (auto num : nums) {
-            if (is_prime(num))
-                result.push_back(num);
+                // Increments the counter keeping track of threads that were assigned work
+                threadsActive++;
+            }
+
+            // Loop to garbage collect all the threads
+            for (int currentThreadIndex = 0; currentThreadIndex < threadsActive; currentThreadIndex++) {
+                // Creates a pointer that will store the resulting boolean information from the thread
+                void *threadResult = nullptr;
+
+                // Closes the thread and stores its returned result
+                pthread_join(threadsArray[currentThreadIndex], &threadResult);
+
+//                printf("%d\n", bool(threadResult));
+
+                // Adds the number the thread worked on to the result vector if it was a prime number
+                if (bool(threadResult))
+                    result.push_back(nums[nextNumberToCheck - threadsActive + currentThreadIndex]);
+            }
         }
     }
     return result;
