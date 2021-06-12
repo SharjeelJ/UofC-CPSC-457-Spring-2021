07759560b2866d0b4cf8c03861f71aea5bc07bf4
Sharjeel Junaid <sharjeelj@outlook.com>
2021-06-11 20:09:44 -0600

    Updated deadlock_detector.cpp - Added in most of the provided hint skeleton code - Only thing remaining is likely just to populate the result object

--- Assignment4/deadlock-detect/deadlock_detector.cpp
+++ Assignment4/deadlock-detect/deadlock_detector.cpp
@@ -1,7 +1,13 @@
-// this is the ONLY file you should edit and submit to D2L
-
 #include "deadlock_detector.h"
 #include "common.h"
+
+using namespace std;
+
+class FastGraph {
+public:
+    vector<vector<int>> adjacencyList;
+    vector<int> outDegree;
+};
 
 /// this is the function you need to (re)implement
 ///
@@ -21,9 +27,97 @@
 /// return Result with edge_index = -1 and empty dl_procs[].
 ///
 Result detect_deadlock(const std::vector<std::string> &edges) {
-    // let's try to guess the results :)
+    // Initialize a vector that will store the results (what edge caused a deadlock and which processes are in deadlock)
     Result result;
-    result.dl_procs = split("12 7 7");
-    result.edge_index = 6;
+
+    // Creates a new graph object that will store the adjacency list based on the passed in input
+    FastGraph graph;
+
+    // Initialize a stringConverter object whose job would be to take in the passed in strings and convert them to unique integers instead
+    Word2Int stringConverter;
+
+    // Loops through all the edges provided and populates the graph (adjacency list and out degree vector)
+    for (int counter = 0; counter < edges.size(); counter++) {
+        // Vector to store parts of the current string being parsed (in the form of process, operator, resource)
+        vector<string> cleanedStringParts = split(simplify(edges[counter]));
+
+        // Stores the current string's process, operator and resource node data
+        int process = stringConverter.get("p" + cleanedStringParts[0]);
+        string activity = cleanedStringParts[1];
+        int resource = stringConverter.get("r" + cleanedStringParts[2]);
+
+        // Checks to see if 2 or 1 new blank entries need to be added to the graph adjacency list and out degree vector (based on how many unique nodes we will be handling this iteration)
+        if (graph.adjacencyList.size() - process == -1 || graph.adjacencyList.size() - resource == -1) {
+            graph.adjacencyList.push_back(vector<int>());
+            graph.adjacencyList.push_back(vector<int>());
+            graph.outDegree.push_back(0);
+            graph.outDegree.push_back(0);
+        } else if (graph.adjacencyList.size() - process == 0 || graph.adjacencyList.size() - resource == 0) {
+            graph.adjacencyList.push_back(vector<int>());
+            graph.outDegree.push_back(0);
+        }
+
+        // Populates the adjacency list and out degree vector appropriately based on whether the current string is an request or assignment operation
+        if (activity == "->") {
+            graph.adjacencyList[resource].push_back(process);
+            graph.outDegree[process]++;
+        } else if (activity == "<-") {
+            graph.adjacencyList[process].push_back(resource);
+            graph.outDegree[resource]++;
+        }
+
+        // TODO: Remove test print
+//        printf("%s\n", edges[counter].c_str());
+        printf("%d %s %d\n\n", process, activity.c_str(), resource);
+    }
+
+    // Stores a local copy of the graph's out degree vector so that it can be modified
+//    vector<int> out = graph.outDegree;
+
+    // Creates and populates a vector that will store all nodes with an out degree value of 0
+    vector<int> zeroes;
+    for (int counter = 0; counter < graph.outDegree.size(); counter++)
+        if (graph.outDegree[counter] == 0)
+            zeroes.push_back(counter);
+
+    // Loops through all nodes that had an out degree value of zero
+    while (!zeroes.empty()) {
+        // Pops and stores the last element from the zeroes list
+        int zeroNode = zeroes.back();
+        zeroes.pop_back();
+
+        for (int currentNode : graph.adjacencyList[zeroNode]) {
+            graph.outDegree[currentNode]--;
+            if (graph.outDegree[currentNode] == 0)
+                zeroes.push_back(currentNode);
+        }
+    }
+
+    for (int counter = 0; counter < edges.size(); counter++) {
+        // Vector to store parts of the current string being parsed (in the form of process, operator, resource)
+        vector<string> cleanedStringParts = split(simplify(edges[counter]));
+
+        // Stores the current string's process, operator and resource node data
+        int process = stringConverter.get("p" + cleanedStringParts[0]);
+
+        if (graph.outDegree[process] > 0)
+            result.dl_procs.push_back(cleanedStringParts[0]);
+    }
+
+    /*
+    out = outDegree;
+    zeroes[] = find all nodes in graph with outdegree == 0
+     while zeroes is not empty:
+        n = remove one entry from zeroes[]
+    for every n2 of adjacencyList[n]:
+         out[n2] --
+         if out[n2] == 0:
+            append n2 to zeroes[]
+    dl_procs[] = all nodes n that represent a process and out[n]>0
+    */
+
+    if (result.dl_procs.size() == 0)
+        result.edge_index = -1;
+
     return result;
 }
