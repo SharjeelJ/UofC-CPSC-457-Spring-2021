a0a060e31ab77e042b27b57ce76d9f62d4b2826c
Sharjeel Junaid <sharjeelj@outlook.com>
2021-06-12 04:36:36 -0600

    Updated scheduler.cpp - Initial skeleton code implementation

--- Assignment4/scheduler/scheduler.cpp
+++ Assignment4/scheduler/scheduler.cpp
@@ -1,5 +1,7 @@
 #include "scheduler.h"
 #include "common.h"
+#include <queue>
+#include <iostream>
 
 using namespace std;
 
@@ -26,10 +28,74 @@ void simulate_rr(
         std::vector<int> &seq
 ) {
     // replace the wrong implementation below with your own!!!!
+//    seq.clear();
+//    for (auto &p : processes) {
+//        p.finish_time = p.arrival_time + p.burst;
+//        p.start_time = p.arrival_time;
+//        seq.push_back(p.id);
+//    }
+
     seq.clear();
-    for (auto &p : processes) {
-        p.finish_time = p.arrival_time + p.burst;
-        p.start_time = p.arrival_time;
-        seq.push_back(p.id);
+
+    int processOnCPU = -1; // Note processes ids start at 0, let -1 denote "idle"
+    int64_t burstRemaining = 0;
+
+    queue<Process> readyQueue;
+    int64_t currentTime = 0;
+    int jobsArrived = 0;
+    int jobsRemaining = processes.size();
+
+    while (1) {
+        if (jobsRemaining == 0) break;
+
+        // Check: if process on CPU is done
+        if (processOnCPU > -1 && burstRemaining == 0) {
+            processOnCPU = -1;
+            jobsRemaining--;
+            continue;
+        }
+
+        // Check: if a new process is arriving
+        if (processes.size() > 0 && processes[jobsArrived].arrival_time == currentTime) {
+            readyQueue.push(processes[jobsArrived]);
+            jobsArrived++;
+            continue;
+        }
+
+        // Check: if CPU is idle and ready queue is not empty
+        if (processOnCPU == -1 && !readyQueue.empty()) {
+            processOnCPU = readyQueue.front().id;
+            burstRemaining = readyQueue.front().burst;
+            readyQueue.pop();
+            continue;
+        }
+
+        // Update the execution order if needed
+        if (seq.empty() || seq.back() != processOnCPU) {
+            seq.push_back(processOnCPU);
+        }
+
+        // Print the current item on CPU
+        if (processOnCPU >= 0) cout << "T" << currentTime << ":\t P" << processOnCPU << endl;
+        else cout << "T" << currentTime << ":\t Idle" << std::endl;
+
+        // Perform a CPU burst then increment the time
+        if (burstRemaining > 0) burstRemaining--;
+        currentTime++;
     }
+
+    // Print the execution order
+    bool printComma = false;
+    std::cout << "Execution order: ";
+    for (auto p : seq) {
+
+        if (printComma) std::cout << ", ";
+        else printComma = true;
+
+        if (p >= 0) std::cout << "P" << p;
+        else std::cout << "Idle";
+
+    }
+    std::cout << std::endl;
 }
+
