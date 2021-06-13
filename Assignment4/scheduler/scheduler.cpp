a5f8a6a4393663aaa6254dd21fec3456b13667fc
Sharjeel Junaid <sharjeelj@outlook.com>
2021-06-12 20:04:44 -0600

    Updated scheduler.cpp - Removed all remaining usage of continue - Implemented a vector that will store the remaining time for each process (this will be checked / modified now instead of burst)

--- Assignment4/scheduler/scheduler.cpp
+++ Assignment4/scheduler/scheduler.cpp
@@ -39,13 +39,17 @@ void simulate_rr(
     // Integer to keep track of how much time the current process has spent on the CPU
     int64_t timeSpentOnCPU = 0;
 
+    // Vector to store the remaining time of each job in the queue
+    vector<int> processesRemainingTime;
+    processesRemainingTime.resize(processes.size());
+
     // Runs until all processes have been completed
     while (true) {
         // If there are no processes remaining then breaks the loop as we are done
         if (processesRemaining == 0) break;
 
         // Checks to see if current process on the CPU is done and replaces it with IDLE if it is
-        if (currentProcess.id > -1 && currentProcess.burst == 0) {
+        if (currentProcess.id > -1 && processesRemainingTime[currentProcess.id] == 0) {
             processes[currentProcess.id].finish_time = currentTime;
             currentProcess = Process();
             timeSpentOnCPU = 0;
@@ -61,15 +65,17 @@ void simulate_rr(
                 processes[currentProcess.id].start_time = currentTime;
             readyQueue.pop();
         }
-            // Optimization 2
-        else if (currentProcess.id > -1 && readyQueue.empty() && processesRemaining == 1 && currentProcess.burst > 1) {
-            currentTime += currentProcess.burst - 1;
-            currentProcess.burst = 1;
+            // If the current process is the last remaining process then skips to end time - 1 (implements optimization hint 2)
+        else if (currentProcess.id > -1 && readyQueue.empty() && processesRemaining == 1 &&
+                 processesRemainingTime[currentProcess.id] > 1) {
+            currentTime += processesRemainingTime[currentProcess.id] - 1;
+            processesRemainingTime[currentProcess.id] = 1;
         }
 
         // Checks to see if there are any incoming processes and adds them to the ready queue if they have arrived
         if (processes.size() > 0 && processes[processesArrived].arrival_time == currentTime) {
             readyQueue.push(processes[processesArrived]);
+            processesRemainingTime[processes[processesArrived].id] = processes[processesArrived].burst;
             processesArrived++;
         }
 
@@ -81,33 +87,36 @@ void simulate_rr(
                 processes[currentProcess.id].start_time = currentTime;
             timeSpentOnCPU = 0;
             readyQueue.pop();
-//            continue;
         }
-            // Optimization 3
+            // If the CPU is currently idle and there will be a process arriving in the future then skips to arrival time - 1 (implements optimization hint 3)
         else if (currentProcess.id == -1 && readyQueue.empty() && processesRemaining >= 1) {
             currentTime = processes[processesArrived].arrival_time - 1;
             seq.push_back(-1);
-//            continue;
         }
 
-        // TODO: Check Optimization 1
-        if (currentProcess.id > -1 && currentProcess.burst <= quantum && processesRemaining > 1) {
-            if (currentTime + currentProcess.burst <= processes[processesArrived].arrival_time && readyQueue.empty()) {
-                currentTime += currentProcess.burst - 1;
-                currentProcess.burst -= currentProcess.burst - 1;
-                timeSpentOnCPU += currentProcess.burst - 1;
+        // TODO: Check Optimization hint 1
+        if (currentProcess.id > -1 && processesRemainingTime[currentProcess.id] <= quantum && processesRemaining > 1) {
+            if (currentTime + processesRemainingTime[currentProcess.id] <= processes[processesArrived].arrival_time &&
+                readyQueue.empty()) {
+                currentTime += processesRemainingTime[currentProcess.id] - 1;
+                processesRemainingTime[currentProcess.id] -= processesRemainingTime[currentProcess.id] - 1;
+                timeSpentOnCPU += processesRemainingTime[currentProcess.id] - 1;
                 printf("HELLO1\n");
-            } else if (currentTime + currentProcess.burst <= processes[processesArrived].arrival_time &&
-                       currentTime + currentProcess.burst <= quantum && !readyQueue.empty()) {
-                currentTime += currentProcess.burst - 1;
-                currentProcess.burst -= currentProcess.burst - 1;
-                timeSpentOnCPU += currentProcess.burst - 1;
+            } else if (currentTime + processesRemainingTime[currentProcess.id] <=
+                       processes[processesArrived].arrival_time &&
+                       currentTime + processesRemainingTime[currentProcess.id] <= quantum && !readyQueue.empty()) {
+                currentTime += processesRemainingTime[currentProcess.id] - 1;
+                processesRemainingTime[currentProcess.id] -= processesRemainingTime[currentProcess.id] - 1;
+                timeSpentOnCPU += processesRemainingTime[currentProcess.id] - 1;
                 printf("HELLO2\n");
-            } else if (currentTime + currentProcess.burst >= processes[processesArrived].arrival_time &&
-                       currentTime + currentProcess.burst <= quantum && !readyQueue.empty()) {
-                currentTime += currentProcess.burst - processes[processesArrived].arrival_time - 1;
-                currentProcess.burst = currentProcess.burst - processes[processesArrived].arrival_time - 1;
-                timeSpentOnCPU += currentProcess.burst - processes[processesArrived].arrival_time - 1;
+            } else if (currentTime + processesRemainingTime[currentProcess.id] >=
+                       processes[processesArrived].arrival_time &&
+                       currentTime + processesRemainingTime[currentProcess.id] <= quantum && !readyQueue.empty()) {
+                currentTime += processesRemainingTime[currentProcess.id] - processes[processesArrived].arrival_time - 1;
+                processesRemainingTime[currentProcess.id] =
+                        processesRemainingTime[currentProcess.id] - processes[processesArrived].arrival_time - 1;
+                timeSpentOnCPU +=
+                        processesRemainingTime[currentProcess.id] - processes[processesArrived].arrival_time - 1;
                 printf("HELLO3\n");
             }
         }
@@ -118,11 +127,12 @@ void simulate_rr(
         }
 
         // Print the current item on CPU
-        printf("%d) P:%d S:%d B:%d\n", currentTime, currentProcess.id, timeSpentOnCPU, currentProcess.burst);
-//        printf("%d\n%d\n%d\n%d\n\n", currentProcess.id, currentTime, readyQueue.size(), currentProcess.burst);
+        printf("%d) P:%d S:%d B:%d\n", currentTime, currentProcess.id, timeSpentOnCPU,
+               processesRemainingTime[currentProcess.id]);
+//        printf("%d\n%d\n%d\n%d\n\n", currentProcess.id, currentTime, readyQueue.size(), processesRemainingTime[currentProcess.id]);
 
         // Goes forward a CPU burst and increments the time spent
-        if (currentProcess.burst > 0) currentProcess.burst--;
+        if (processesRemainingTime[currentProcess.id] > 0) processesRemainingTime[currentProcess.id]--;
         currentTime++;
         timeSpentOnCPU++;
     }
