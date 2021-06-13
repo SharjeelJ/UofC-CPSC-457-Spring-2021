d2f3ea559212bf685549e734f101a415d9e0042d
Sharjeel Junaid <sharjeelj@outlook.com>
2021-06-13 14:39:51 -0600

    Updated scheduler.cpp - Implemented optimization hint 1 (properly tested)

--- Assignment4/scheduler/scheduler.cpp
+++ Assignment4/scheduler/scheduler.cpp
@@ -94,18 +94,34 @@ void simulate_rr(
 
         // If the current process's remaining time is less than a quantum - time elapsed on CPU, then skips to either the arrival time of the next process or the end of the slice based on the smaller value (implements optimization hint 1)
         if (currentProcessID > -1 && remainingTime <= abs(timeOnCPU - quantum)) {
-            if (abs(currentTime - processes[processesArrived].arrival_time) <= abs(timeOnCPU - quantum)) {
+            // TODO FIX
+            if (abs(currentTime - processes[processesArrived].arrival_time) <= abs(timeOnCPU - quantum) &&
+                abs(currentTime - processes[processesArrived].arrival_time) <= remainingTime) {
+                printf("OPT1.1\n");
                 remainingTime -= abs(currentTime - processes[processesArrived].arrival_time);
                 timeOnCPU += abs(currentTime - processes[processesArrived].arrival_time);
-                currentTime += abs(currentTime - processes[processesArrived].arrival_time);
+                currentTime = processes[processesArrived].arrival_time;
+                if ((seq.empty() || seq.back() != currentProcessID) && int64_t(seq.size()) < max_seq_len)
+                    seq.push_back(currentProcessID);
+                jumpOccurred = true;
+                continue;
+            }
+                // TODO Check
+            else if (abs(currentTime - processes[processesArrived].arrival_time) <= abs(timeOnCPU - quantum) &&
+                     abs(currentTime - processes[processesArrived].arrival_time) > remainingTime) {
+                printf("OPT1.2\n");
+                currentTime += remainingTime;
+                timeOnCPU += remainingTime;
+                remainingTime = 0;
                 if ((seq.empty() || seq.back() != currentProcessID) && int64_t(seq.size()) < max_seq_len)
                     seq.push_back(currentProcessID);
                 jumpOccurred = true;
                 continue;
             } else if (abs(currentTime - processes[processesArrived].arrival_time) > abs(timeOnCPU - quantum)) {
-                currentTime += abs(timeOnCPU - quantum);
-                timeOnCPU += abs(timeOnCPU - quantum);
-                remainingTime = abs(timeOnCPU - quantum);
+                printf("OPT1.3\n");
+                currentTime += remainingTime;
+                timeOnCPU += remainingTime;
+                remainingTime = 0;
                 if ((seq.empty() || seq.back() != currentProcessID) && int64_t(seq.size()) < max_seq_len)
                     seq.push_back(currentProcessID);
                 jumpOccurred = true;
@@ -115,6 +131,7 @@ void simulate_rr(
 
         // If the current process is the last remaining process then skips to the process's end time (implements optimization hint 2)
         if (currentProcessID > -1 && processesRemaining == 1) {
+            printf("OPT2\n");
             currentTime += remainingTime;
             timeOnCPU += remainingTime;
             remainingTime = 0;
@@ -126,6 +143,7 @@ void simulate_rr(
 
         // If the CPU is currently idle and there will be a process arriving in the future then skips to its arrival time (implements optimization hint 3)
         if (currentProcessID == -1 && readyQueue.empty()) {
+            printf("OPT3\n");
             currentTime = processes[processesArrived].arrival_time;
             if ((seq.empty() || seq.back() != currentProcessID) && int64_t(seq.size()) < max_seq_len)
                 seq.push_back(currentProcessID);
@@ -135,15 +153,17 @@ void simulate_rr(
 
         // If there is no processes waiting in the ready queue then skips either to the arrival time of the next process or the end time of the current process based on the smaller value (implements optimization hint 4)
         if (currentProcessID > -1 && readyQueue.empty()) {
-            if (abs(currentTime - processes[processesArrived].arrival_time) <= remainingTime) {
+            if (currentTime + remainingTime > processes[processesArrived].arrival_time) {
+                printf("OPT4.1\n");
                 remainingTime -= abs(currentTime - processes[processesArrived].arrival_time);
                 timeOnCPU += abs(currentTime - processes[processesArrived].arrival_time);
-                currentTime += abs(currentTime - processes[processesArrived].arrival_time);
+                currentTime = processes[processesArrived].arrival_time;
                 if ((seq.empty() || seq.back() != currentProcessID) && int64_t(seq.size()) < max_seq_len)
                     seq.push_back(currentProcessID);
                 jumpOccurred = true;
                 continue;
-            } else if (abs(currentTime - processes[processesArrived].arrival_time) > remainingTime) {
+            } else if (currentTime + remainingTime <= processes[processesArrived].arrival_time) {
+                printf("OPT4.2\n");
                 currentTime += remainingTime;
                 timeOnCPU += remainingTime;
                 remainingTime = 0;
